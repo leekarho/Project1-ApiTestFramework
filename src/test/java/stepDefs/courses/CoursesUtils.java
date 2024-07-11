2409ec4c759c2e832c53126afd19349a38501899
Kar-ho Lee <k-h_lee@hotmail.co.uk>
2024-07-11 12:22:11 +0100

    add parseResponseToJSONarray in utils

--- src/test/java/stepDefs/courses/CoursesUtils.java
+++ src/test/java/stepDefs/courses/CoursesUtils.java
@@ -1,11 +1,25 @@
 package stepDefs.courses;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CoursesUtils {
 
+    public static JSONArray parseResponseToJsonArray(Response response){
 
+        JSONArray responseBody = null;
+        JSONParser parser = new JSONParser();
+        try{
+            responseBody = (JSONArray) parser.parse(response.getBody().asString());
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return responseBody;
+    }
 
     public static RequestSpecification getCoursesRequestSpec(String baseUri, String path) {
         return new RequestSpecBuilder()
--- src/test/java/stepDefs/courses/GetCoursesTest.java
+++ src/test/java/stepDefs/courses/GetCoursesTest.java
@@ -1,16 +1,20 @@
 package stepDefs.courses;
 
+import io.cucumber.cienvironment.internal.com.eclipsesource.json.JsonArray;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.simple.JSONArray;
 import pojos.Course;
 import stepDefs.config.TestConfig;
 
 public class GetCoursesTest {
     private static final String BASE_URI = TestConfig.getBaseUri();
     private static final String PATH = TestConfig.getCoursesPath();
-    Response response;
+    private static Response response;
+    private static JSONArray responseBody;
     private static Course[] courses;
 
     @Given("the courses endpoint is available")
@@ -24,5 +28,11 @@ public class GetCoursesTest {
                 .when()
                 .get()
                 .thenReturn();
+
+        responseBody = CoursesUtils.parseResponseToJsonArray(response);
+    }
+
+    @And("the response should include an array of all courses")
+    public void theResponseShouldIncludeAnArrayOfAllCourses() {
     }
 }
